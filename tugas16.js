c34ebdad66e982ac436455a2124f4956b6e9c528
ali <alboyzskeaters@gmail.com>
2020-01-09 21:31:27 +0700

    Tugas16JS

---
+++ tugas16.js
@@ -0,0 +1,13 @@
+let nilai = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10];
+let hasil = Math.min(...nilai);
+console.log("Nilai Minimal : " + hasil);
+let hasilx = Math.max(...nilai);
+console.log("Nilai Maximal : " + hasilx);
+
+console.log("================");
+
+let nilai1 = [45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5 ];
+let hasil1 = Math.min(...nilai1);
+console.log("Nilai1 Minimal : " + hasil1);
+let hasilx1 = Math.max(...nilai1);
+console.log("Nilai1 Maximal : " + hasilx1);
